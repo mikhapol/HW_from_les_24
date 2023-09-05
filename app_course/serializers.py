6a7e8d920b9d657ed3e009e34a9f333ef54c45ed
mikhapol_linux <mikhapol@bk.ru>
2023-09-05 20:39:23 +0300

    Убрал лишнее

--- app_course/serializers.py
+++ app_course/serializers.py
@@ -6,22 +6,11 @@ from app_lesson.serializers import LessonSerializer
 class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для модели курса"""
     lessons_count = serializers.SerializerMethodField()  # Тип поля отвечающий за описание подсчёта количества уроков.
-    # lessons = LessonSerializer(many=True, source='lesson')  # Тип поля отвечающий за вывод уроков.
-    lessons = serializers.SerializerMethodField(
-        read_only=True)  # добавляем поле уроков и делаем метод для получения уроков
+    lessons = LessonSerializer(many=True, source='lesson', read_only=True)  # Тип поля отвечающий за вывод уроков.
 
     def get_lessons_count(self, instance):
         """Метод отвечающий за подсчёт"""
         return instance.lesson.count()
-
-    def get_lessons(self, obj):
-        """Добавление уроков для курса"""
-        lesson_list = []
-        if obj.lesson.all():
-            for i in obj.lesson.all().values_list():  # проходимся циклом по всем связанным урокам
-                lesson_list.append  # добавляем в список урок
-                return lesson_list
-        return None
 
     class Meta:
         model = Course
