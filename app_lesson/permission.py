1ff895f632a98f822459b453a4d51e0654a79876
mikhapol_linux <mikhapol@bk.ru>
2023-09-10 12:08:47 +0300

    25.1 Права доступа в DRF. Дополнил Permissions

--- app_lesson/permission.py
+++ app_lesson/permission.py
@@ -1,16 +1,33 @@
 from rest_framework.permissions import BasePermission
 
+from app_users.models import UserRoles
+
 
 class IsOwner(BasePermission):
-    """Права доступа для суперюзера."""
+    """Права доступа для владельца."""
+    message = "Вы не являетесь владельцем."
+
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner
+        if request.user == obj.owner:
+            return True
+        return False
 
 
 class IsStaff(BasePermission):
+    """Модератор может все кроме удаления и создания объектов."""
+    message = "Вы не являетесь модератором."
+
     def has_permission(self, request, view):
-        """Модератор может все кроме удаления и создания объектов."""
-        if request.user.is_staff:
-            if request.method in ['DELETE', 'POST']:
-                return False
+        """
+        Два варианта
+        1 - Вариант без админки
+        2 - Вариант с админкой - Active
+        """
+        # if request.user.is_staff:
+        #     if request.method in ['DELETE', 'POST']:
+        #         return False
+        #     return True
+
+        if request.user.role == UserRoles.MODERATOR:
             return True
+        return False
