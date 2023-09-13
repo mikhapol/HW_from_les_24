b6c69208b0085a81c445728fb388d7bc94025e65
mikhapol_linux <mikhapol@bk.ru>
2023-09-13 19:55:13 +0300

    25.2 Валидаторы, пагинация и тесты.

--- app_course/serializers.py
+++ app_course/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from app_course.models import Course
+from app_course.models import Course, Subscription
+from app_lesson.models import Lesson
 from app_lesson.serializers import LessonSerializer
 
 
@@ -7,11 +8,31 @@ class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для модели курса"""
     lessons_count = serializers.SerializerMethodField()  # Тип поля отвечающий за описание подсчёта количества уроков.
     lessons = LessonSerializer(many=True, source='lesson', read_only=True)  # Тип поля отвечающий за вывод уроков.
+    is_subscribed = serializers.SerializerMethodField()  # Проверка подписки
 
-    def get_lessons_count(self, instance):
-        """Метод отвечающий за подсчёт"""
-        return instance.lesson.count()
+    def get_lessons_count(self, course):
+        """Метод для получения количества уроков в курсе"""
+
+        return Lesson.objects.filter(course=course).count()
+
+    def get_lessons(self, course):
+        """Метод для получения списка всех уроков в курсе"""
+
+        return [lesson.title for lesson in course.lesson_set.all()]
+
+    def get_is_subscribed(self, course):
+        """Метод для проверки подписки к курсу"""
+
+        return Subscription.objects.filter(course=course, user=self.context['request'].user).exists()
 
     class Meta:
         model = Course
+        fields = '__all__'
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели подписки"""
+
+    class Meta:
+        model = Subscription
         fields = '__all__'
--- config/settings.py
+++ config/settings.py
@@ -163,4 +163,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
         # 'rest_framework.permissions.AllowAny',
     ),
+    # 'DEFAULT_PAGINATION_CLASS':
+    #     'rest_framework.pagination. LimitOffsetPagination',
+    #     'PAGE_SIZE': 100
 }
